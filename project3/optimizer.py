a853edda4a6abf53e69df4fab54af2192c52fc12
EthanSeal <ejseal21@colby.edu>
2019-11-12 17:58:33 -0500

    rmsprop

--- project3/optimizer.py
+++ project3/optimizer.py
@@ -35,9 +35,41 @@ class Optimizer():
             return SGD_Momentum(**kwargs)
         elif name.lower() == 'adam':
             return Adam(**kwargs)
+        elif name.lower() == 'rmsprop':
+            return RMSProp(**kwargs)
         else:
             raise ValueError('Unknown optimizer name!')
 
+
+class RMSProp(Optimizer):
+    '''Update weights using RMSProp update rule
+        '''
+    def __init__(self, lr, beta):
+        '''
+        Paramters:
+        ----------
+        lr: float > 0. Learning rate.
+        beta: float 0 <= beta <= 1
+        '''
+        
+        self.lr = lr
+        self.beta = beta
+        self.moving_avg = 0.0
+
+    def update_weights(self):
+        '''
+        Updates weights according to RMSProp and returns a deep copy of the 
+        updated weights for this time step.
+
+        Returns:
+        ----------
+        A copy of the updated weights for this time step
+
+        Citation: https://towardsdatascience.com/understanding-rmsprop-faster-neural-network-learning-62e116fcf29a
+        '''
+        self.moving_avg = self.beta * self.moving_avg + (1 - self.beta) * self.d_wts * self.d_wts
+        self.wts = self.wts - (self.lr / sqrt(self.moving_avg)) * self.d_wts
+        return self.wts.copy()
 
 class SGD(Optimizer):
     '''Update weights using Stochastic Gradient Descent (SGD) update rule.
@@ -57,13 +89,9 @@ class SGD(Optimizer):
         Returns:
         -----------
         A COPY of the updated weights for this time step.
-
-        TODO: Write the SGD weight update rule.
-        See notebook for review of equations.
         '''
 
         self.wts = self.wts - self.lr*self.d_wts
-
         return self.wts.copy()
         
 
